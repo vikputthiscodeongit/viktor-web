2056e998c056493d61fa0f94a031aae4c6d9d4d3
vikputthiscodeongit <16013785+vikputthiscodeongit@users.noreply.github.com>
2022-02-07 18:58:05 +0100

    Improve devLabel styling

--- wp-content/themes/viktor-web/src/js/index.js
+++ wp-content/themes/viktor-web/src/js/index.js
@@ -188,8 +188,8 @@ import stylesheet from "../scss/style.scss";
         el.style.cssText = `
         position: fixed; bottom: 0.25rem; right: 0.25rem; z-index: 10000;
         padding: 1rem;
-        text-transform: uppercase; font-weight: 700;
-        background-color: white; border: 0.125rem solid red;
+        text-transform: uppercase; font-size: 1.25rem; font-weight: 700;
+        background-color: white; border: 0.25rem solid red;
         `;
         el.textContent = "Build: dev";
 
