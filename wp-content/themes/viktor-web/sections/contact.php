c27d772b03acec23ba0cadd1eb37e085edb7765d
vikputthiscodeongit <16013785+vikputthiscodeongit@users.noreply.github.com>
2022-03-29 16:45:25 +0200

    Move individual sections to their own partial

---
+++ wp-content/themes/viktor-web/sections/contact.php
@@ -0,0 +1,23 @@
+<section class="site-section" id="contact">
+    <div class="container">
+        <div class="section-title">
+            <h1>Contact</h1>
+        </div>
+
+        <div class="section-content">
+            <p>You can connect with me on one of the following services.</p>
+
+            <div>
+                <a href="https://www.linkedin.com/in/viktor-cks/" target="_blank" rel="noopener">
+                    <img class="icon" src="<?php echo THEME_DIR_URI; ?>/dist/images/static/icons/icon-linkedin-color-64x64.png" alt="LinkedIn">
+                </a>
+            </div>
+        </div>
+
+        <div class="section-content">
+            <p>To get in touch directly, fill in the form below.</p>
+
+            <?php echo do_shortcode('[contact-form-7 id="5" title="Contact form 1" html_class="form form--contact"]'); ?>
+        </div>
+    </div>
+</section>
