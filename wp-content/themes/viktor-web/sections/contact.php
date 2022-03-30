544d4926685f9a923cef06c4d5c28369a18b4f0f
vikputthiscodeongit <16013785+vikputthiscodeongit@users.noreply.github.com>
2022-03-30 02:28:54 +0200

    Change Contact layout

--- wp-content/themes/viktor-web/sections/contact.php
+++ wp-content/themes/viktor-web/sections/contact.php
@@ -5,18 +5,34 @@
         </div>
 
         <div class="section-content">
-            <p>You can connect with me on one of the following services.</p>
+            <ul class="icon-grid">
+                <li class="icon">
+                    <a href="https://www.instagram.com/picsmadebyvik/" target="_blank" rel="noopener">
+                        <img class="icon" src="<?php echo THEME_DIR_URI; ?>/dist/images/icons/instagram-logo.svg" alt="Instagram">
+                    </a>
+                </li>
 
-            <div>
-                <a href="https://www.linkedin.com/in/viktor-cks/" target="_blank" rel="noopener">
-                    <img class="icon" src="<?php echo THEME_DIR_URI; ?>/dist/images/static/icons/icon-linkedin-color-64x64.png" alt="LinkedIn">
-                </a>
-            </div>
+                <li class="icon">
+                    <a href="https://www.linkedin.com/in/viktor-cks/" target="_blank" rel="noopener">
+                        <img class="icon" src="<?php echo THEME_DIR_URI; ?>/dist/images/icons/linkedin-logo.svg" alt="LinkedIn">
+                    </a>
+                </li>
+
+                <li class="icon icon--shift-right">
+                    <a href="https://github.com/vikputthiscodeongit" target="_blank" rel="noopener">
+                        <img class="icon" src="<?php echo THEME_DIR_URI; ?>/dist/images/icons/github-logo.svg" alt="GitHub">
+                    </a>
+                </li>
+
+                <li class="icon">
+                    <a href="https://www.npmjs.com/search?q=%40codebundlesbyvik" target="_blank" rel="noopener">
+                        <img class="icon" src="<?php echo THEME_DIR_URI; ?>/dist/images/icons/npm-logo.svg" alt="npm">
+                    </a>
+                </li>
+            </ul>
         </div>
 
         <div class="section-content">
-            <p>To get in touch directly, fill in the form below.</p>
-
             <?php echo do_shortcode('[contact-form-7 id="5" title="Contact form 1" html_class="form form--contact"]'); ?>
         </div>
     </div>
