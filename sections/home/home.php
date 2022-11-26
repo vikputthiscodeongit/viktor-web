b295281f97f88480a20c0aca786e98ba4dc17c50
vikputthiscodeongit <16013785+vikputthiscodeongit@users.noreply.github.com>
2022-11-26 15:29:32 +0100

    Include forgotten home navigation

--- sections/home/home.php
+++ sections/home/home.php
@@ -2,6 +2,8 @@
     <div class="container">
 		<?php include __DIR__ . "/../../components/about-me/about-me-view.php"; ?>
 
+		<?php include __DIR__ . "/../../components/nav/home-nav-view.php"; ?>
+
 		<?php include __DIR__ . "/../../components/blob/blob-1.php"; ?>
 
 		<?php include __DIR__ . "/../../components/blob/blob-2.php"; ?>
