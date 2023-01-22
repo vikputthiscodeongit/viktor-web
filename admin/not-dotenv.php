36e3ad48e5710768e727f8e9a722fd11245fe1e2
vikputthiscodeongit <16013785+vikputthiscodeongit@users.noreply.github.com>
2023-01-22 17:15:41 +0100

    Remove the secrets

--- admin/not-dotenv.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("DEV_MODE", false);
-define("WEBSITE_DOMAIN", DEV_MODE ? "viktor-web.local" : "viktorchin.nl");
-define("EMAIL_ADDRESS_PERSONAL", "mail@viktorchin.nl");
-define("EMAIL_ADDRESS_WEBMASTER", "webmaster@viktorchin.nl");
