be6b10a4eecd95316c6f4cdb3f9e945582ad542f
Viktor <code@viktorchin.nl>
2023-03-14 00:59:39 +0100

    Better typing for returnStatus form helper

--- components/form/helpers.php
+++ components/form/helpers.php
@@ -1,6 +1,6 @@
 <?php
-function returnStatus($status, array|false $values = false) {
-    http_response_code($status instanceof \UnitEnum ? $status->value : $status);
+function returnStatus(FormSubmitStatusses|int $status, array|false $values = false) {
+    http_response_code($status instanceof \FormSubmitStatusses ? $status->value : $status);
     header("Content-Type: application/json; charset=utf-8");
     echo json_encode($values);
 
