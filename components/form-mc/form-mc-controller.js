4d221dd875114ba9e34f129114d7ef8a9f48ecb3
Viktor <code@viktorchin.nl>
2023-04-20 21:13:25 +0200

    Allow for subsequent form mc initialization

--- components/form-mc/form-mc-controller.js
+++ components/form-mc/form-mc-controller.js
@@ -1,37 +1,63 @@
 import createEl from "/helpers/create-el";
 
-export default async function initFormMc(mcEl) {
+export default async function initFormMc(mcEl, mcLabelEl, loaderEl, initFormMcButtonEl) {
     console.log("initFormMc(): Running.");
 
-    mcEl.setAttribute("disabled", true);
+    const freshLoad = !mcLabelEl && !loaderEl && !initFormMcButtonEl;
+    console.log(freshLoad);
 
-    const mcLabelEl = createEl("label", { for: mcEl.name });
-    const loaderEl = createEl("div", { class: "spinner", style: "font-size: 1.5rem;" });
+    if (freshLoad) {
+        mcEl.setAttribute("disabled", true);
+
+        loaderEl = createEl("div", { class: "spinner", style: "font-size: 1.5rem;" });
+        mcLabelEl = createEl("label", { for: mcEl.name });
+    } else {
+        initFormMcButtonEl.setAttribute("disabled", true);
+    }
+
+    mcEl.parentElement.appendChild(loaderEl);
 
     try {
         // throw new Error();
-        await makeProblem(0, mcEl, mcLabelEl, loaderEl);
+        await makeProblem(-1, mcEl, mcLabelEl, loaderEl);
 
-        mcEl.parentElement.insertBefore(mcLabelEl, mcEl);
-        mcEl.removeAttribute("disabled");
+        if (freshLoad) {
+            mcEl.parentElement.insertBefore(mcLabelEl, mcEl);
+            mcEl.removeAttribute("disabled");
+        } else {
+            initFormMcButtonEl.remove();
+            mcLabelEl.parentElement.appendChild(mcEl);
+        }
+
+        loaderEl.remove();
 
         return true;
     } catch (error) {
         console.error(error);
 
+        initFormMcButtonEl.removeAttribute("disabled");
+
         return error;
     }
+
 }
 
-const MAKE_PROBLEM_TIMEOUTS = [3000, 5000, 8000];
+// DEBUG VALUES
+const MAKE_PROBLEM_TIMEOUTS = [3000 / 3, 5000 / 3, 8000 / 3];
 
 async function makeProblem(tryCount, mcEl, mcLabelEl, loaderEl) {
     console.log("makeProblem(): Running.");
+    console.log(tryCount);
+    console.log(mcEl);
+    console.log(mcLabelEl);
+    console.log(loaderEl);
 
     tryCount++;
     console.log(`makeProblem(): tryCount - ${tryCount}`);
 
-    mcEl.parentElement.appendChild(loaderEl);
+    if (tryCount > 0) {
+        mcEl.parentElement.appendChild(loaderEl);
+    }
 
     try {
         const problem = await getMathsProblem();
@@ -44,17 +70,23 @@ async function makeProblem(tryCount, mcEl, mcLabelEl, loaderEl) {
 
         mcLabelEl.textContent = `${problem[0]} + ${problem[1]} =`;
 
-        loaderEl.remove();
+        if (tryCount > 0) {
+            loaderEl.remove();
+        }
     } catch (error) {
         console.error(error);
 
         if (tryCount > MAKE_PROBLEM_TIMEOUTS.length) {
             console.error(`makeProblem(): Failed to make problem ${tryCount} times. Exiting.`);
 
+            addInitFormMcButton(mcEl, mcLabelEl, loaderEl);
+
+            loaderEl.remove();
+
             return;
         }
 
-        setTimeout(scheduleMakeProblem, MAKE_PROBLEM_TIMEOUTS[tryCount - 1], 0, tryCount, mcEl, mcLabelEl, loaderEl);
+        setTimeout(scheduleMakeProblem, MAKE_PROBLEM_TIMEOUTS[tryCount > 0 ? tryCount - 1 : 0], 0, tryCount, mcEl, mcLabelEl, loaderEl);
     }
 }
 
@@ -97,4 +129,14 @@ function scheduleMakeProblem(invalidAfter, tryCount, mcEl, mcLabelEl, loaderEl) 
     console.log(`scheduleMakeProblem(): timeToRefresh 2 - ${timeToRefresh}`);
 
     setTimeout(makeProblem, timeToRefresh, tryCount, mcEl, mcLabelEl, loaderEl);
+}
+
+function addInitFormMcButton(mcEl, mcLabelEl, loaderEl) {
+    mcEl.remove();
+
+    mcLabelEl.textContent = "CAPTCHA inactive";
+
+    const initFormMcButtonEl = createEl("button", { type: "button", text: "Activate" });
+    initFormMcButtonEl.addEventListener("click", () => initFormMc(mcEl, mcLabelEl, loaderEl, initFormMcButtonEl));
+    mcLabelEl.parentElement.appendChild(initFormMcButtonEl);
 }
