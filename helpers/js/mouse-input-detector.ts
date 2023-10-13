a53a8fc56ac689c572fc8fe6f00a7163ea3296d4
Viktor <code@viktorchin.nl>
2023-10-13 02:07:06 +0200

    Inline mouse-input-detector code

--- helpers/js/mouse-input-detector.ts
+++
@@ -1,9 +0,0 @@
-export default function initMouseInputDetector(targetEl: Element) {
-    if (!targetEl) {
-        console.error("initMouseInputDetector(): Aborting initialization - targetEl undefined or not found!");
-        return;
-    }
-
-    targetEl.addEventListener("mousedown", () => targetEl.classList.add("using-mouse"));
-    targetEl.addEventListener("keydown", () => targetEl.classList.remove("using-mouse"));
-}
--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import htmlDevLabel from "@codebundlesbyvik/html-dev-label";
-import initMouseInputDetector from "./helpers/js/mouse-input-detector";
 import initTypeItAboutMe from "./components/sections/home/about-me/typeit";
 import initForm from "./components/sections/contact/form/form-controller";
 import "./style.scss";
 
 document.documentElement.classList.replace("js-disabled", "js-enabled");
-
-initMouseInputDetector(document.body);
+document.body.addEventListener("mousedown", () => document.body.classList.add("using-mouse"));
+document.body.addEventListener("keydown", () => document.body.classList.remove("using-mouse"));
 
 htmlDevLabel();
 
