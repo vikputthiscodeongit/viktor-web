45dfe60e81d5af3d9684558ddc36948e227ac48f
Viktor <code@viktorchin.nl>
2024-12-16 00:51:21 +0100

    feat(TypeIt): Various minor improvements

--- components/sections/home/about-me/typeit.ts
+++ components/sections/home/about-me/typeit.ts
@@ -1,38 +1,32 @@
-import { El, Options, TypeItInstance } from "typeit/dist/types";
+import { El, Options } from "typeit/dist/types";
 import TypeIt from "typeit";
 
-export default function initTypeItAboutMe(targetEl: Element | null) {
-    if (!targetEl) {
-        console.error("initTypeItAboutMe(): Aborting initialization - targetEl undefined or not found!");
-
-        return;
-    }
-
+export default function initTypeItAboutMe(targetEl: Element) {
     const options: Options = {
         speed: 75,
         deleteSpeed: 40,
-        loop: true
+        loop: true,
     };
 
-    const instance: TypeItInstance = new (TypeIt as any)(targetEl as El, options)
+    const instance = new TypeIt(targetEl as El, options)
         //
-        .type("Photographer", {delay: 1800})
-        .delete(null, {delay: 1000})
+        .type("Photographer", { delay: 1800 })
+        .delete(undefined, { delay: 1000 })
         //
-        .type("Web devlo", {delay: 500})
-        .move(-2, {speed: 150, delay: 350})
-        .type("e", {delay: 500})
-        .move(2, {speed: 100, delay: 400})
-        .type("per", {delay: 1850})
-        .delete(null, {delay: 1000})
+        .type("Web devlo", { delay: 500 })
+        .move(-2, { speed: 150, delay: 350 })
+        .type("e", { delay: 500 })
+        .move(2, { speed: 100, delay: 400 })
+        .type("per", { delay: 1850 })
+        .delete(undefined, { delay: 1000 })
         //
-        .type("Moti", {delay: 400})
-        .delete(2, {speed: 120, delay: 350})
-        .type("toring enthusiast", {delay: 2000})
-        .delete(null, {delay: 900})
+        .type("Moti", { delay: 400 })
+        .delete(2, { speed: 120, delay: 350 })
+        .type("toring enthusiast", { delay: 2000 })
+        .delete(undefined, { delay: 900 })
         //
-        .type("Human", {delay: 1450})
-        .delete(null, {delay: 900})
+        .type("Human", { delay: 1450 })
+        .delete(undefined, { delay: 900 })
         //
         .go();
 
