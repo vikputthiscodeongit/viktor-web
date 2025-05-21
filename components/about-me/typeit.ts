86c03aac0f6377f29b7a16c9399966484f73e1de
Viktor <code@viktorchin.nl>
2025-05-21 23:31:32 +0200

    Update "About me" content

--- components/about-me/typeit.ts
+++ components/about-me/typeit.ts
@@ -3,20 +3,20 @@ import TypeIt from "typeit";
 
 export default function initTypeItAboutMe(targetEl: Element) {
     const instance = new TypeIt(targetEl as El, {
-        speed: 75,
+        speed: 80,
         deleteSpeed: 40,
         loop: true,
     })
         //
-        .type("Photographer", { delay: 1800 })
-        .delete(undefined, { delay: 1000 })
-        //
-        .type("Web devlo", { delay: 500 })
-        .move(-2, { speed: 150, delay: 350 })
+        .type("Software devlo", { delay: 500 })
+        .move(-2, { delay: 350 })
         .type("e", { delay: 500 })
-        .move(2, { speed: 100, delay: 400 })
+        .move(2, { delay: 400 })
         .type("per", { delay: 1850 })
-        .delete(undefined, { delay: 1000 })
+        .delete(undefined, { delay: 900 })
+        //
+        .type("Photographer", { delay: 1800 })
+        .delete(undefined, { delay: 900 })
         //
         .type("Moti", { delay: 400 })
         .delete(2, { speed: 120, delay: 350 })
