82213f63e28d4cce7268f4c6729d36b3b78df6ce
Viktor <code@viktorchin.nl>
2025-05-27 13:59:54 +0200

    feat(SimpleMathsCaptcha): Improve problem generation

--- php/controllers/simple-maths-captcha-generator.php
+++ php/controllers/simple-maths-captcha-generator.php
@@ -1,14 +1,19 @@
 <?php
 require_once __DIR__ . "/../../global.php";
 require_once ROOT_DIR . "/session.php";
-require_once ROOT_DIR . "/php/controllers/simple-maths-captcha-global.php";
 
-function makeSimpleMathsCaptchaProblem($base_id)
+function makeSimpleMathsCaptchaProblem($base_id, $valid_for_time)
 {
-    $invalid_after_time = (int) floor(microtime(true) * 1000) + SIMPLE_MATHS_CAPTCHA_PROBLEM_INVALID_AFTER_TIME_MS;
+    $invalid_after_time = (int) floor(microtime(true) * 1000) + $valid_for_time;
     $problem_data = [rand(1, 9), rand(1, 9), $invalid_after_time];
 
-    $_SESSION[$base_id] = $problem_data;
+    $simple_maths_captcha_id = "simple-maths-captcha";
+
+    if ($base_id) {
+        $simple_maths_captcha_id = $base_id . "-" . $simple_maths_captcha_id;
+    }
+
+    $_SESSION[$simple_maths_captcha_id] = $problem_data;
 
     return $problem_data;
 }
