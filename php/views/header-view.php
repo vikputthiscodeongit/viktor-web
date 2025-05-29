1ab039bcca7f9cdd5e14ccc2f452a87551067bb2
Viktor <code@viktorchin.nl>
2025-05-29 22:26:46 +0200

    refactor(Header): Use root relative paths

--- php/views/header-view.php
+++ php/views/header-view.php
@@ -41,11 +41,11 @@
     <script src="./dist/index.js" defer></script>
 
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="./public/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./public/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./public/favicon/favicon-16x16.png">
-    <link rel="manifest" href="./public/favicon/site.webmanifest">
-    <link rel="mask-icon" href="./public/favicon/safari-pinned-tab.svg" color="#000000">
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/public/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/public/favicon/safari-pinned-tab.svg" color="#000000">
     <meta name="msapplication-TileColor" content="#000000">
     <meta name="theme-color" content="#000000">
 </head>
