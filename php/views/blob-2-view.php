945e1d1d1d0810dea570f5c02d45a774b3476b56
Viktor <code@viktorchin.nl>
2025-07-13 19:36:01 +0200

    perf(Blob): Increase fetch priority

--- php/views/blob-2-view.php
+++ php/views/blob-2-view.php
@@ -1,3 +1,3 @@
 <div class="blob blob--right" aria-hidden="true">
-    <img src="/public/images/blobs/blob-2.svg" alt="">
+    <img src="/public/images/blobs/blob-2.svg" alt="" fetchpriority="high">
 </div>
