be4b7266ba62982ca324a399c515f2ff8b9d2f94
vikputthiscodeongit <code@viktorchin.nl>
2025-11-02 18:08:06 +0100

    feat(AboutMe): Update content

--- ts/controllers/typeit-controller.ts
+++ ts/controllers/typeit-controller.ts
@@ -8,35 +8,35 @@ export default function initTypeItAboutMe(targetEl: HTMLElement) {
     const instance = new TypeIt(targetEl, {
         speed: 80,
         deleteSpeed: 40,
+        startDelay: 400,
         loop: true,
+        loopDelay: 2000,
     })
         //
-        .type("Likes technlo", { delay: 500 })
-        .move(-2, { delay: 350 })
-        .type("o", { delay: 300 })
-        .move(2, { delay: 400 })
-        .type("gy ...", { delay: 1500 })
-        .move(-3, { delay: 333 })
-        .delete(undefined, { delay: 900 })
-        .move(3, { delay: 450 })
+        .type("Likes technlo", { delay: 350 })
+        .move(-2, { delay: 300 })
+        .type("o", { delay: 200 })
+        .move(2, { delay: 300 })
+        .type("gy", { delay: 2100 })
+        .delete(10, { delay: 900 })
         //
-        .type(" and cars", { delay: 1650 })
-        .delete(4, { delay: 900 })
+        .type("cars", { delay: 1950 })
+        .delete(4, { delay: 850 })
         //
-        .type("photography", { delay: 1800 })
-        .delete(15, { delay: 900 })
+        .type("photography", { delay: 2200 })
+        .delete(11, { delay: 950 })
         //
-        .type("but also goodfood", { delay: 300 })
-        .move(-4, { delay: 350 })
+        .type("goodfood", { delay: 400 })
+        .move(-4, { delay: 300 })
         .type(" ", { delay: 250 })
-        .move(4, { delay: 1450 })
-        .delete(9, { delay: 900 })
+        .move(4, { delay: 1950 })
+        .delete(9, { delay: 850 })
         //
-        .type("music", { delay: 1600 })
-        .delete(14, { delay: 900 })
+        .type("music", { delay: 2050 })
+        .delete(5, { delay: 1000 })
         //
-        .type("and nature", { delay: 1400 })
-        .delete(undefined, { delay: 900 })
+        .type("nature", { delay: 2000 })
+        .delete(undefined)
         //
         .go();
 
