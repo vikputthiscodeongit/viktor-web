81c70072d05ad6a40b86e7734889f3346129b564
Viktor <code@viktorchin.nl>
2025-11-04 14:15:23 +0100

    feat(ContactForm): Improve CAPTCHA invalidation time accuracy

--- php/controllers/simple-maths-captcha/Generate.php
+++ php/controllers/simple-maths-captcha/Generate.php
@@ -20,14 +20,16 @@ class Generate
     {
         $digit_1 = rand(1, 9);
         $digit_2 = rand(1, 9);
-        $invalid_after_time = (int) floor(microtime(true) * 1000) + $valid_for_time;
+        $generation_time = (int) floor(microtime(true) * 1000);
 
+        $invalid_after_time = (int) $generation_time + $valid_for_time;
         $_SESSION[$this->id] = [$digit_1, $digit_2, $invalid_after_time];
 
         return [
-            "digit_1" => $digit_1,
-            "digit_2" => $digit_2,
-            "invalid_after" => $invalid_after_time
+            $digit_1,
+            $digit_2,
+            $generation_time,
+            $valid_for_time
         ];
     }
 }
