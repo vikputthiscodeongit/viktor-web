vikputthiscodeongit/viktor-web

commit 9993b0d3c487f8a9780cc1a05385c1b4dc85cd2f

Form controller improvements
BE
* Fix: Required form controls without other validation attributes can be empty
* Feat: Align validation error types with ValidityState
* Refactor: Integrate custom validators as plugins
* Refactor: Minor field name changes

FE
* Fix: Show CAPTCHA activator button validation border
* Refactor: Split form controller file

ts/controllers/contact-form/index.ts
@@ -1,0 +1,242 @@
+// TODO:
+// * Fade out Simple Notifications when #contact not in viewport.
+
+import type SimpleMathsCaptcha from "@codebundlesbyvik/simple-maths-captcha";
+import { fetchWithTimeout } from "@codebundlesbyvik/js-helpers";
+import SimpleNotifier from "@codebundlesbyvik/simple-notifier";
+import { UPROCESSABLE_CONTENT } from "../../helpers/http-status-codes";
+import { initSimpleMathsCaptcha } from "./captcha-init";
+import { initStoredFormDataLoader, makeFormDataObject } from "./store-data";
+import { clearValidation, updateValidation } from "./validate";
+
+type UnprocessableContentData = {
+    message: string;
+    invalid_controls: {
+        id: string;
+        errors: string[];
+    }[];
+};
+
+const NotificationContent = {
+    messageStored: {
+        text: [
+            "Your message has been stored on your device.",
+            "You may close this page and come back at a later time to retry sending it.",
+        ],
+        variant: "warning",
+        hideAfterTime: 6000,
+    },
+    submitted: {
+        title: "Message sent",
+        text: "I'll be in touch soon!",
+        variant: "success",
+    },
+    unknownError: {
+        text: "Failed to send message. Please try again at a later time.",
+        variant: "error",
+    },
+    validationError: {
+        text: "One or more fields failed validation. Please check and correct them.",
+        variant: "warning",
+    },
+};
+
+async function submitForm({
+    formEl,
+    captcha,
+    notifier,
+}: {
+    formEl: HTMLFormElement;
+    captcha?: SimpleMathsCaptcha;
+    notifier: SimpleNotifier;
+}) {
+    console.info("submitForm: Running...");
+
+    let submitSuccessful = false;
+
+    const formFieldsets = formEl.querySelectorAll<HTMLFieldSetElement>("fieldset");
+    const formControlElsWithoutButtons = formEl.querySelectorAll<
+        HTMLInputElement | HTMLTextAreaElement
+    >("input:not([type=button], [type=reset], [type=submit]), textarea");
+
+    try {
+        formFieldsets.forEach((el) => {
+            el.setAttribute("disabled", "disabled");
+        });
+
+        const formData = new FormData();
+
+        formControlElsWithoutButtons.forEach((el) => {
+            formData.set(el.name, el.value);
+        });
+
+        const response = await fetchWithTimeout("/api/form/submit-contact-form.php", {
+            method: "POST",
+            body: formData,
+        });
+
+        if (!response.ok) {
+            if (response.status === UPROCESSABLE_CONTENT) {
+                const data = (await response.json()) as unknown;
+
+                const isValidData = (data: unknown): data is UnprocessableContentData =>
+                    typeof data === "object" &&
+                    data !== null &&
+                    "invalid_controls" in data &&
+                    Array.isArray(data.invalid_controls);
+
+                let invalidCaptchaEl = null;
+
+                if (isValidData(data) && data.invalid_controls.length > 0) {
+                    const scrollTarget = document.querySelector(`#${data.invalid_controls[0].id}`);
+
+                    if (scrollTarget) {
+                        document.documentElement.classList.add("has-scroll-offset");
+                        scrollTarget.scrollIntoView();
+                        document.documentElement.classList.remove("has-scroll-offset");
+                    }
+
+                    if (captcha) {
+                        for (const el of [captcha.activatorButtonEl, captcha.answerInputEl]) {
+                            if (!data.invalid_controls.find((control) => control.id === el.id))
+                                continue;
+
+                            invalidCaptchaEl = el;
+
+                            break;
+                        }
+                    }
+                }
+
+                if (captcha) {
+                    captcha.answerInputEl.setCustomValidity(invalidCaptchaEl ? "required" : "");
+                }
+
+                notifier.show(NotificationContent.validationError);
+            } else {
+                const formDataObjExcludedKeys = [];
+
+                if (captcha) {
+                    captcha.answerInputEl.setCustomValidity("");
+
+                    formDataObjExcludedKeys.push(
+                        captcha.answerInputEl.id,
+                        captcha.digit1InputEl.id,
+                        captcha.digit2InputEl.id,
+                    );
+                }
+
+                localStorage.setItem(
+                    `${formEl.name}-data`,
+                    JSON.stringify(makeFormDataObject(formData, formDataObjExcludedKeys)),
+                );
+
+                notifier.show(NotificationContent.unknownError);
+                notifier.show({
+                    ...NotificationContent.messageStored,
+                    hideOlder: false,
+                });
+            }
+
+            return;
+        }
+
+        submitSuccessful = true;
+
+        notifier.show(NotificationContent.submitted);
+
+        localStorage.removeItem(`${formEl.name}-data`);
+        formControlElsWithoutButtons.forEach((el) => (el.value = ""));
+
+        return;
+    } catch (error) {
+        notifier.show(NotificationContent.unknownError);
+
+        throw error instanceof Error ? error : new Error("Unknown error during form submission!");
+    } finally {
+        formFieldsets.forEach((el) => el.removeAttribute("disabled"));
+
+        if (!submitSuccessful) {
+            const formControlElsToUpdate = formEl.querySelectorAll<
+                HTMLButtonElement | HTMLInputElement | HTMLTextAreaElement
+            >(
+                "button[type=button], input:not([type=hidden], [type=reset], [type=submit]), textarea",
+            );
+            for (const [, el] of formControlElsToUpdate.entries()) {
+                if (
+                    el.getAttribute("required") === null &&
+                    el.getAttribute("data-required") !== "true"
+                )
+                    continue;
+
+                if (!captcha || el.id === captcha.activatorButtonEl.id) {
+                    el.setAttribute("data-validation-on-input", "true");
+                }
+
+                updateValidation(el);
+            }
+        } else {
+            formControlElsWithoutButtons.forEach((el) => clearValidation(el));
+            if (captcha) captcha.deactivate();
+        }
+    }
+}
+
+export default function initContactForm(formEl: HTMLFormElement) {
+    console.info("initContactForm: Running...");
+
+    const submitButtonEl = formEl.querySelector("[type=submit]");
+
+    if (!submitButtonEl) {
+        throw new Error("submitButtonEl not found!");
+    }
+
+    const captcha = initSimpleMathsCaptcha(formEl);
+    const notifier = new SimpleNotifier({ hideOlder: true });
+
+    const storedFormData = localStorage.getItem(`${formEl.name}-data`);
+
+    if (storedFormData) {
+        initStoredFormDataLoader(storedFormData, formEl, notifier);
+    }
+
+    const formControlElsWithoutButtonsOrHidden = formEl.querySelectorAll<
+        HTMLInputElement | HTMLTextAreaElement
+    >("input:not([type=button], [type=hidden], [type=reset], [type=submit]), textarea");
+    formControlElsWithoutButtonsOrHidden.forEach((el) => {
+        // TODO: Debounce
+        el.addEventListener("input", () => {
+            el.setAttribute("data-had-interaction", "true");
+
+            if (el.getAttribute("data-validation-on-input") === "true") {
+                updateValidation(el);
+            }
+
+            return;
+        });
+
+        el.addEventListener("blur", () => {
+            if (el.getAttribute("data-had-interaction") === "true") {
+                el.setAttribute("data-validation-on-input", "true");
+            }
+
+            if (el.getAttribute("data-validation-on-input") === "true") {
+                updateValidation(el);
+            }
+
+            return;
+        });
+    });
+
+    submitButtonEl.addEventListener("click", (e) => {
+        e.preventDefault();
+        submitForm({ formEl, captcha, notifier }).catch((error) => console.error(error));
+
+        return;
+    });
+
+    formEl.querySelectorAll("fieldset:disabled").forEach((el) => el.removeAttribute("disabled"));
+    formEl.classList.remove("has-overlay");
+
+    return;
+}

index.ts
@@ -1,6 +1,6 @@
 import htmlDevLabel from "@codebundlesbyvik/html-dev-label";
 import initTypeItAboutMe from "./ts/controllers/typeit-controller";
-import initContactForm from "./ts/controllers/contact-form-controller";
+import initContactForm from "./ts/controllers/contact-form/index";
 import "./style.scss";
 
 (function () {

api/form/submit-contact-form.php
@@ -6,8 +6,11 @@
 require_once __DIR__ . "/../../global.php";
 require_once ROOT_DIR . "/_folder/values.php";
 require_once ROOT_DIR . "/php/helpers/return-http-response.php";
-require_once ROOT_DIR . "/php/controllers/contact-form-controller.php";
 require_once ROOT_DIR . "/content/contact-form-content.php";
+require ROOT_DIR . "/php/controllers/FormController.php";
+require ROOT_DIR . "/php/controllers/simple-maths-captcha/Validate.php";
+
+use SimpleMathsCaptcha\Validate as CaptchaValidator;
 
 function sendMail($form_values)
 {
@@ -75,20 +78,24 @@ function requestHandler($form_name, $form_items)
             returnHttpResponse(HttpStatus::METHOD_NOT_ALLOWED, null, [["Allow" => "POST"]]);
         }
 
-        $clean_form_data = sanitizeFormData($_POST);
+        $form_controller = new FormController(
+            $form_name,
+            $form_items,
+            [new CaptchaValidator($form_name . "-simple-maths-captcha")]
+        );
+
+        $clean_form_data = $form_controller->sanitizeData($_POST);
         // var_dump($clean_form_data);
-        $form_controls_attrs = getFormControlsAttributesFromFormItems($form_items);
-        // var_dump($form_controls_attrs);
-        $validated_form_data = validateFormData($form_name, $form_controls_attrs, $clean_form_data);
+        $validated_form_data = $form_controller->validateData($clean_form_data);
         // var_dump($validated_form_data);
-        $invalid_form_controls = getFormControlsErrors($validated_form_data);
+        $invalid_form_controls = $form_controller->getInvalidControls($validated_form_data);
         // var_dump($invalid_form_controls);
 
         if (count($invalid_form_controls) > 0) {
             returnHttpResponse(HttpStatus::UNPROCESSABLE_CONTENT, [
                 "message" => "One or more fields failed validation.",
                 // Use array_values() because of https://www.php.net/manual/en/function.json-encode.php#129803
-                "invalid_form_controls" => array_values($invalid_form_controls)
+                "invalid_controls" => array_values($invalid_form_controls)
             ]);
         }
 

php/controllers/FormController.php
@@ -1,0 +1,143 @@
+<?php
+
+class FormController
+{
+    protected $name = "";
+    private $controls_attrs = [];
+    private $extra_validators = [];
+
+    public function __construct(string $name, array $items, ?array $extra_validators)
+    {
+        $this->name = $name;
+        $this->controls_attrs = $this->getControlsAttrs($items);
+        $this->extra_validators = $extra_validators;
+    }
+
+    private function getControlsAttrs(array $items)
+    {
+        $items_flat = array_merge([], ...$items);
+        $controls_attrs = [];
+
+        foreach ($items_flat as $key => $item) {
+            if ($key === "legend") continue;
+
+            if (isset($item["control"]["type"]) && $item["control"]["type"] === "submit") continue;
+
+            $control_attrs = array_filter(
+                $item["control"],
+                function ($attr) {
+                    if (in_array($attr, ["type", "minlength", "maxlength", "pattern", "required"])) {
+                        return $attr;
+                    }
+                },
+                ARRAY_FILTER_USE_KEY
+            );
+
+            $controls_attrs[$item["control"]["id"]] = $control_attrs;
+        }
+
+        return $controls_attrs;
+    }
+
+    public function sanitizeData(array $form_data)
+    {
+        $clean_form_data = [];
+
+        foreach ($form_data as $control_name => $control_value) {
+            $control_value_trimmed = trim($control_value);
+            $clean_form_data[strval($control_name)] = $control_value_trimmed !== ""
+                ? htmlspecialchars($control_value_trimmed)
+                : "";
+        }
+
+        return $clean_form_data;
+    }
+
+    public function validateData(array $form_data)
+    {
+        $validated_form_data = [];
+
+        foreach ($this->controls_attrs as $control_id => $control_attrs) {
+            if (count($this->extra_validators) > 0) {
+                foreach ($this->extra_validators as $validator) {
+                    if (!$validator->shouldValidate($control_id)) continue;
+
+                    array_push($validated_form_data, $validator->getValidationResult($form_data));
+
+                    // Continue with next form control.
+                    continue 2;
+                }
+            }
+
+            $form_control_validation_result = [
+                "id" => $control_id,
+                "errors" => []
+            ];
+
+            // Form control must have an entry in the form data.
+            if (!array_key_exists($control_id, $form_data)) {
+                $form_control_validation_result["errors"] = ["entry_missing"];
+                array_push($validated_form_data, $form_control_validation_result);
+
+                continue;
+            }
+
+            // Required form control mustn't have an empty value.
+            if (isset($control_attrs["required"]) && $control_attrs["required"] === true && $form_data[$control_id] === "") {
+                $form_control_validation_result["errors"] = ["value_missing"];
+                array_push($validated_form_data, $form_control_validation_result);
+
+                continue;
+            }
+
+            $form_control_value = $form_data[$control_id];
+
+            foreach ($control_attrs as $attr => $attr_value) {
+                if ($attr === "required") continue;
+
+                $condition_name = $attr;
+                $error = false;
+
+                switch ($attr) {
+                    case "type";
+                        if ($attr_value !== "email") break;
+
+                        // RegEx used by browsers for the `email` input type.
+                        // https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
+                        $REGEX = "/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/";
+                        if (preg_match($REGEX, $form_control_value) !== 1) $error = "type_mismatch";
+
+                        break;
+
+                    case "minlength";
+                        if (strlen($form_control_value) > 0 && strlen($form_control_value) < $attr_value) $error = "too_short";
+                        break;
+
+                    case "maxlength";
+                        if (strlen($form_control_value) > $attr_value) $error = "too_long";
+                        break;
+                }
+
+                if (!$error) continue;
+
+                array_push($form_control_validation_result["errors"], $condition_name);
+            }
+
+            array_push($validated_form_data, $form_control_validation_result);
+        }
+
+        return $validated_form_data;
+    }
+
+    /**
+     * @param array<int|string, array{id: string, errors: array<string>}> $validated_form_data
+     */
+    public function getInvalidControls(array $validated_form_data)
+    {
+        $invalid_form_controls = array_filter($validated_form_data, function ($control_data) {
+            return count($control_data["errors"]) > 0;
+        });
+
+        return $invalid_form_controls;
+    }
+}

php/controllers/contact-form-controller.php
@@ -1,146 +1,0 @@
-<?php
-require_once __DIR__ . "/../../global.php";
-require ROOT_DIR . "/php/controllers/simple-maths-captcha/Validate.php";
-
-use SimpleMathsCaptcha\Validate as CaptchaValidator;
-
-function isValidEmailAddress(string $email_address)
-{
-    // RegEx used by browsers for the `email` input type.
-    // https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
-    $REGEX = "/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/";
-
-    return preg_match($REGEX, $email_address) === 1;
-}
-
-function sanitizeFormData($form_data)
-{
-    $clean_form_data = [];
-
-    foreach ($form_data as $form_control_name => $form_control_value) {
-        $form_control_value_trimmed = trim($form_control_value);
-        $clean_form_data[$form_control_name] = $form_control_value_trimmed !== ""
-            ? htmlspecialchars($form_control_value_trimmed)
-            : "";
-    }
-
-    return $clean_form_data;
-}
-
-function getFormControlsAttributesFromFormItems($form_items)
-{
-    $form_items_flat = array_merge([], ...$form_items);
-    $form_controls_attrs = [];
-
-    foreach ($form_items_flat as $key => $item) {
-        if ($key === "legend") continue;
-
-        if (isset($item["control"]["type"]) && $item["control"]["type"] === "submit")
-            continue;
-
-        $form_control_attrs = array_filter(
-            $item["control"],
-            function ($attr) {
-                if (in_array($attr, ["id", "type", "minlength", "maxlength", "pattern", "required"])) {
-                    return $attr;
-                }
-            },
-            ARRAY_FILTER_USE_KEY
-        );
-
-        array_push($form_controls_attrs, $form_control_attrs);
-    }
-
-    return $form_controls_attrs;
-}
-
-function validateFormData($form_name, $form_controls_attrs, $form_data)
-{
-    $captcha_validator = new CaptchaValidator($form_name . "-simple-maths-captcha");
-
-    $validated_form_data = [];
-
-    foreach ($form_controls_attrs as $form_control_attrs) {
-        $form_control_validation_result = [
-            "id" => $form_control_attrs["id"],
-            "validation_errors" => []
-        ];
-
-        // Simple Maths CAPTCHA
-        if ($captcha_validator->isCaptchaFormControlId($form_control_attrs["id"])) {
-            $validation_result = $captcha_validator->getValidationState($form_data);
-
-            if ($validation_result !== "valid") {
-                $form_control_validation_result["id"] =
-                    $captcha_validator->getInvalidFormControlId($validation_result);
-                $form_control_validation_result["validation_errors"] = [true];
-            }
-
-            array_push($validated_form_data, $form_control_validation_result);
-
-            continue;
-        }
-
-        // Each form control must be reflected in the form data.
-        if (!array_key_exists($form_control_attrs["id"], $form_data)) {
-            $form_control_validation_result["validation_errors"] = [true];
-            array_push($validated_form_data, $form_control_validation_result);
-
-            continue;
-        }
-
-        $form_control_value = $form_data[$form_control_attrs["id"]];
-
-        // Empty, non-required form controls.
-        if (
-            $form_control_value === "" &&
-            (!isset($form_control_attrs["required"]) || $form_control_attrs["required"] !== true)
-        ) {
-            array_push($validated_form_data, $form_control_validation_result);
-
-            continue;
-        }
-
-        foreach ($form_control_attrs as $attr => $attr_value) {
-            if ($attr === "id" || $attr === "required") continue;
-
-            $condition_name = $attr;
-            $form_control_value_valid = false;
-
-            switch ($attr) {
-                case "type":
-                    // Continue with next attribute.
-                    if ($attr_value !== "email") continue 2;
-
-                    $condition_name = "email";
-                    $form_control_value_valid = isValidEmailAddress($form_control_value);
-                    break;
-
-                case "minlength";
-                    $form_control_value_valid = strlen($form_control_value) >= $attr_value;
-                    break;
-
-                case "maxlength";
-                    $form_control_value_valid = strlen($form_control_value) <= $attr_value;
-                    break;
-            }
-
-            if ($form_control_value_valid === false) {
-                array_push($form_control_validation_result["validation_errors"], $condition_name);
-            }
-        }
-
-        array_push($validated_form_data, $form_control_validation_result);
-    }
-
-    return $validated_form_data;
-}
-
-function getFormControlsErrors($validated_form_data)
-{
-    $invalid_form_controls = array_filter($validated_form_data, function ($form_control_data) {
-        return count($form_control_data["validation_errors"]) > 0;
-    });
-
-    return $invalid_form_controls;
-}

php/controllers/simple-maths-captcha/Validate.php
@@ -7,29 +7,24 @@ require_once ROOT_DIR . "/session.php";
 
 class Validate
 {
-    protected $id = "simple-maths-captcha";
+    protected $control_id = "simple-maths-captcha";
 
-    public function __construct(?string $id)
+    public function __construct(?string $control_id)
     {
-        if (!$id) return;
+        if (!$control_id) return;
 
-        $this->id = $id;
+        $this->control_id = $control_id;
     }
 
-    public function isCaptchaFormControlId(string $form_control_id)
+    public function shouldValidate(string $control_id)
     {
-        return strpos($form_control_id, $this->id) !== false;
+        return strpos($control_id, $this->control_id) !== false;
     }
 
-    public function getInvalidFormControlId(string $validation_state)
-    {
-        return $validation_state === "invalid" ? $this->id . "-answer" : $this->id . "-activator";
-    }
-
-    public function isAnswerValid(int $digit_1, int $digit_2, int $answer)
+    private function isAnswerValid(int $digit_1, int $digit_2, int $answer)
     {
         // Contains the problem digits and a unix timestamp.
-        $stored_problem_data = $_SESSION[$this->id];
+        $stored_problem_data = $_SESSION[$this->control_id];
 
         if (
             !is_int($digit_1) || !is_int($digit_2) || !is_int($answer) ||
@@ -51,22 +46,29 @@ class Validate
         return $answer_valid;
     }
 
-    public function getValidationState(array $form_data)
+    public function getValidationResult(array $form_data)
     {
-
         $has_all_captcha_data =
-            array_key_exists($this->id . "-digit-1", $form_data) &&
-            array_key_exists($this->id . "-digit-2", $form_data) &&
-            array_key_exists($this->id . "-answer", $form_data);
+            array_key_exists($this->control_id . "-digit-1", $form_data) &&
+            array_key_exists($this->control_id . "-digit-2", $form_data) &&
+            array_key_exists($this->control_id . "-answer", $form_data);
 
         if (!$has_all_captcha_data) {
-            return "inactive";
+            return [
+                "id" => $this->control_id . "-activator",
+                "errors" => ["captcha_inactive"]
+            ];
         }
 
-        return $this->isAnswerValid(
-            (int) $form_data[$this->id . "-digit-1"],
-            (int) $form_data[$this->id . "-digit-2"],
-            (int) $form_data[$this->id . "-answer"],
-        ) ? "valid" : "invalid";
+        $answer_valid = $this->isAnswerValid(
+            (int) $form_data[$this->control_id . "-digit-1"],
+            (int) $form_data[$this->control_id . "-digit-2"],
+            (int) $form_data[$this->control_id . "-answer"],
+        );
+
+        return [
+            "id" => $this->control_id . "-answer",
+            "errors" => !$answer_valid ? ["value_invalid"] : []
+        ];
     }
 }

scss/components/_form.scss
@@ -123,6 +123,40 @@ input {
 
 button,
 input[type="button"],
+input[type="submit"],
+input[type="email"],
+input[type="text"],
+textarea {
+    border: rem(2px) solid;
+    outline: rem(2px) solid transparent;
+    transition: $transition-base;
+    transition-property: background-color, border-color, outline-color;
+
+    &:not(:focus-visible) {
+        &[data-show-validation-border="invalid"] {
+            outline-color: $warning;
+        }
+
+        &[data-show-validation-border="valid"] {
+            outline-color: $success;
+        }
+
+        &[data-show-validation-border="neutral"] {
+            outline-color: $blue;
+        }
+    }
+
+    &:focus-visible {
+        outline-color: $blue;
+
+        @media (prefers-contrast: more) {
+            outline-color: $black;
+        }
+    }
+}
+
+button,
+input[type="button"],
 input[type="submit"] {
     display: inline-flex;
     align-items: center;
@@ -133,15 +167,9 @@ input[type="submit"] {
     font: 500 $font-size-base $headings-font-family;
     color: $white;
     background-color: $blue;
-    border: rem(2px) solid $blue;
+    border-color: $blue;
     border-radius: rem(20px);
-    outline: rem(2px) solid transparent;
     outline-offset: rem(4px);
-    // TODO: Use $transition-base for all animations?
-    transition:
-        background-color 0.2s ease-in-out,
-        border-color 0.2s ease-in-out,
-        outline-color $transition-base;
 
     &:hover {
         &:not(:disabled) {
@@ -151,14 +179,6 @@ input[type="submit"] {
 
         &:disabled {
             cursor: default;
-        }
-    }
-
-    &:focus-visible {
-        outline-color: $blue;
-
-        @media (prefers-contrast: more) {
-            outline-color: $black;
         }
     }
 
@@ -185,32 +205,8 @@ textarea {
     line-height: $line-height-base;
     font-size: $font-size-base;
     background-color: $off-white-dark;
-    border: rem(2px) solid transparent;
+    border-color: transparent;
     border-radius: rem(4px);
-    outline: rem(2px) solid transparent;
-    transition: outline-color $transition-base;
-
-    &:not(:focus-visible) {
-        &[data-show-validation-border="invalid"] {
-            outline-color: $warning;
-        }
-
-        &[data-show-validation-border="valid"] {
-            outline-color: $success;
-        }
-
-        &[data-show-validation-border="neutral"] {
-            outline-color: $blue;
-        }
-    }
-
-    &:focus-visible {
-        outline-color: $blue;
-
-        @media (prefers-contrast: more) {
-            outline-color: $black;
-        }
-    }
 
     .field--ifl & {
         padding-top: rem(31px);

commit 11307e69168729aa5013ebc44b5ff0978fd5face

fix(ContactForm): Rolling field validation after submit
I broke it in 9993b0d3c487f8a9780cc1a05385c1b4dc85cd2f.

ts/controllers/contact-form/index.ts
@@ -169,7 +169,7 @@ async function submitForm({
                 )
                     continue;
 
-                if (!captcha || el.id === captcha.activatorButtonEl.id) {
+                if (!captcha || (captcha && el.id !== captcha.activatorButtonEl.id)) {
                     el.setAttribute("data-validation-on-input", "true");
                 }
 